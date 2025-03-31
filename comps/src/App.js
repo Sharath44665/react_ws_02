7a9be75d76bad13f619a4f54c1e3a8e5a55f5eca
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-03-31 21:47:13 +0530

    demo on link

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,6 +1,9 @@
+import Link from "./components/Link";
 
 function App() { 
     return <>
+    <Link to='/accordion'> <span className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Go accordion</span></Link>
+    <Link to='/dropdown'><span className="font-medium text-blue-600 dark:text-blue-500 hover:underline">show dropdown</span></Link>
     </>
 }
 

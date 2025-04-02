4840650b132774c91a29039ce790c9102ae8987b
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-04-02 06:10:59 +0530

    open links on ctrl or meta key

--- comps/src/components/Link.js
+++ comps/src/components/Link.js
@@ -5,11 +5,12 @@ const Link = ({to, children}) => {
     const { navigate } = useContext(NavigationContext)
 
     const handleClick = (event) => {
+        if (event.metaKey || event.ctrlKey) return;
         event.preventDefault();
         navigate(to)
     }
     return <>
-    <a onClick={handleClick}>{children} </a>
+    <a href={to} onClick={handleClick}>{children} </a>
     </>
 }
 

ecd262fcb82315b002bd2da7c35706bb8d6dfe58
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-04-02 20:22:01 +0530

    show modal content on click

--- comps/src/pages/ModalPage.js
+++ comps/src/pages/ModalPage.js
@@ -1,8 +1,17 @@
-import Modal from '../components/Modal'
+import Modal from '../components/Modal';
+import Button from '../components/Button';
+import { useState } from 'react';
 
 const ModalPage = () => {
+    const [showModal, SetShowModal] = useState(false);
+
+    const handleClick = () => {
+        SetShowModal(true) 
+    }
     return <>
-    <Modal/>
+    <Button primary onClick={handleClick} > Open Modal</Button>
+    {showModal && <Modal/>}
+    
     </>
 }
 

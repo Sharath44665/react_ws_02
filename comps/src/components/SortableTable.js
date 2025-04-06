dd8363a44244ce023d6d3a27281356025cacaf69
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-04-06 20:28:23 +0530

    sort table and show icons

--- comps/src/components/SortableTable.js
+++ comps/src/components/SortableTable.js
@@ -1,27 +1,28 @@
 import { useState } from "react";
 import Table from "./Table";
+import { FaAngleUp, FaAngleDown } from "react-icons/fa";
 
 const SortableTable = (props) => {
     const [sortOrder, setSortOrder] = useState(null)
     const [sortBy, setSortBy] = useState(null);
     const { config, data } = props;
 
-        const handleClick = (label) => {
-            console.log(label)
-            if (sortOrder === null) {
-                setSortOrder('asc')
-                setSortBy(label)
-            }
-            else if (sortOrder === 'asc') {
-                setSortOrder('desc')
-                setSortBy(label)
-            }
-            else if (sortOrder === 'desc'){
-                setSortOrder(null);
-                setSortBy(null)
-            }
-            
+    const handleClick = (label) => {
+        console.log(label)
+        if (sortOrder === null) {
+            setSortOrder('asc')
+            setSortBy(label)
         }
+        else if (sortOrder === 'asc') {
+            setSortOrder('desc')
+            setSortBy(label)
+        }
+        else if (sortOrder === 'desc') {
+            setSortOrder(null);
+            setSortBy(null)
+        }
+
+    }
 
 
     const updatedConfig = config.map((col, idx) => {
@@ -31,8 +32,10 @@ const SortableTable = (props) => {
         }
         return {
             ...col,
-            header: () => <th onClick={() => handleClick(col.label)}>{col.label} 
-            {getIcons(col.label, sortBy, sortOrder)} </th>,
+            header: () => <th className="flex items-center cursor-pointer hover:bg-gray-300" onClick={() => handleClick(col.label)}>{col.label}
+                
+                    {getIcons(col.label, sortBy, sortOrder)}
+                 </th>,
 
         }
     })
@@ -42,15 +45,15 @@ const SortableTable = (props) => {
     // find the correct sortValue function and use it for sorting
     let sortedData = data;
 
-    if (sortOrder && sortBy){
-        const {sortValue} = config.find((column) => column.label === sortBy)
+    if (sortOrder && sortBy) {
+        const { sortValue } = config.find((column) => column.label === sortBy)
         sortedData = [...data].sort((a, b) => {
             const valueA = sortValue(a)
             const valueB = sortValue(b)
 
-            const reverseOrder = sortOrder === 'asc'? 1 : -1;
+            const reverseOrder = sortOrder === 'asc' ? 1 : -1;
 
-            if (typeof valueA === 'string'){
+            if (typeof valueA === 'string') {
                 return valueA.localeCompare(valueB) * reverseOrder;
             }
             else {
@@ -59,29 +62,39 @@ const SortableTable = (props) => {
         })
     }
     return <>
-    <div>
-        {sortOrder} === {sortBy}
-        <Table {...props} data={sortedData} config={updatedConfig} />
+        <div>
+            {sortOrder} === {sortBy}
+            <Table {...props} data={sortedData} config={updatedConfig} />
 
-    </div>
+        </div>
 
     </>
 
 }
 
 const getIcons = (label, sortBy, sortOrder) => {
-    if (label !== sortBy){
-        return 'show both icons'
+    if (label !== sortBy) {
+        return <div>
+            <FaAngleUp />
+            <FaAngleDown />
+        </div>
     }
 
-    if (sortOrder === null){
-        return 'show both icons'
+    if (sortOrder === null) {
+        return <div>
+            <FaAngleUp />
+            <FaAngleDown />
+        </div>
     }
-    else if (sortOrder === 'asc'){
-        return 'show up icon'
+    else if (sortOrder === 'asc') {
+        return <div>
+            <FaAngleUp />
+        </div>
     }
-    else if (sortOrder === 'desc'){
-        return 'show down icon'
+    else if (sortOrder === 'desc') {
+        return <div>
+            <FaAngleDown />
+        </div>
     }
 }
 export default SortableTable; 

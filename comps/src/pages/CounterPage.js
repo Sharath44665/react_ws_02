9d9677e3a74ed8046b3eedc67b5e6e41969c0aac
sharath44665 <sharathwelcomesyouin@gmail.com>
2025-04-07 23:59:37 +0530

    adding new state updates

--- comps/src/pages/CounterPage.js
+++ comps/src/pages/CounterPage.js
@@ -4,6 +4,8 @@ import Panel from '../components/Panel'
 
 const INCREMENT_COUNT = 'increment'
 const SET_VALUE_TO_ADD = 'change-value-to-add'
+const DECREMENT_COUNT = 'decrement'
+const ADD_VALUE_TO_COUNT = 'add-value-to-count'
 
 const reducer = (state, action) => {
     switch(action.type){
@@ -11,6 +13,17 @@ const reducer = (state, action) => {
             return {
                 ...state,
                 count: state.count+1
+            }
+        case DECREMENT_COUNT:
+            return {
+                ...state,
+                count: state.count -1
+            }
+        case ADD_VALUE_TO_COUNT:
+            return {
+                ...state,
+                count: state.count + state.valueToAdd,
+                valueToAdd: 0,
             }
         case SET_VALUE_TO_ADD:
             return {
@@ -42,8 +55,9 @@ const CounterPage = ({ initialCount }) => {
     }
 
     const decrement = () => {
-        
-        // setCount(count - 1) 
+        dispatch({
+            type: DECREMENT_COUNT
+        }) 
     }
 
     const handleChange = (event) => {
@@ -56,9 +70,10 @@ const CounterPage = ({ initialCount }) => {
 
     const handleSubmit = (event) => {
         event.preventDefault()
-
-        // setCount(count + valueToAdd)
-        // setValueToAdd(0)
+        dispatch({
+            type: ADD_VALUE_TO_COUNT
+        })
+ 
     }
     return <>
         <Panel className='m-3'>

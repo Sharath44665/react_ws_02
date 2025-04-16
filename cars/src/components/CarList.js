de58bafac86ebda8db0a7be58950d7cdf0e757db
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-04-17 04:47:35 +0530

    add and show car in ui

--- cars/src/components/CarList.js
+++ cars/src/components/CarList.js
@@ -4,8 +4,22 @@ const CarList = () => {
     const cars = useSelector((state) => {
         return state.cars.data;
     })
+
+    const handleDelete = (car) => {
+
+    }
+
+    const renderedCars = cars.map((car, idx) => {
+        return (<div key={idx} className="panel">
+            <p>{car.name} - ₹{car.cost} </p>
+            <button className="button is-danger" onClick={() => handleDelete(car)}>Delete</button>
+        </div>)
+    })
     console.log(cars)
-    return <div>CarList</div>
+    return <div className="car-list">
+        {renderedCars}
+        <hr />
+    </div>
 }
 
 

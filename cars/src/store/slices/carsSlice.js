c8ccf1aee68357735912274578063ba79b44621c
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-04-16 22:08:19 +0530

    add carsSlice

---
+++ cars/src/store/slices/carsSlice.js
@@ -0,0 +1,37 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+
+const carsSlice = createSlice({
+    name: 'cars',
+    initialState: {
+        searchTearm: '',
+        cars: []
+    },
+    reducers: {
+        changeSearchTearm: (state, action) => {
+            state.searchTearm = action.payload
+        },
+        addCar: (state, action) => {
+            // action.payload === {name: 'some name', cost: 333}
+            state.cars.push({
+                name: action.payload.name,
+                cost: action.payload.cost,
+                id: nanoid, // auto generated random id from redux
+            })
+
+        },
+        removeCar: (state, action) => {
+            // action.payload === remove the car with id
+
+            const updated = state.cars.filter((car) => {
+                return car.id !== action.payload
+            });
+            state.cars === updated
+        },
+
+    }
+
+})
+
+export const {changeSearchTearm, addCar, removeCar } = carsSlice.actions;
+export const carsrReducer = carsSlice.reducer;

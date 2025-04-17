68d3920f3cf830105454c466d484bd1729d67240
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-04-17 06:04:14 +0530

    add searchTearm

--- cars/src/components/CarSearch.js
+++ cars/src/components/CarSearch.js
@@ -1,5 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
+import {changeSearchTearm} from '../store';
+
 const CarSearch = () => {
-    return <div>Car search</div>
+    const dispatch = useDispatch()
+
+    const searchTerm = useSelector((state) => {
+        return state.cars.searchTerm
+    })
+
+    const handleSearchTermChange = (event) => {
+        dispatch(changeSearchTearm(event.target.value))
+    }
+
+    return <div className="list-header">
+        <h3 className="title is-3">My cars</h3>
+        <div className="search field is-horizontal">
+            <label className="label">Search</label>
+            <input className="input" value={searchTerm} onChange={handleSearchTermChange} />
+        </div>
+    </div>
 }
 
 

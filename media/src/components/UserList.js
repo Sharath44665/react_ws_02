ee6c114a26862ad16014b032977f21f8c4489611
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-04-19 06:30:36 +0530

    show loading from response

--- media/src/components/UserList.js
+++ media/src/components/UserList.js
@@ -1,6 +1,29 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers} from '../store'
+
 const UserList = () => {
+    const dispatch = useDispatch();
+
+    const {isLoading, data, error} = useSelector((state) => {
+        return state.users;
+    })
+
+    useEffect (() => {
+        dispatch(fetchUsers())
+    }, [dispatch])
+
+
+    if ( isLoading ){
+        return <div>Loading...</div>
+    }
+
+    if (error){
+        return <div>Error: something went wrong</div>
+    }
+
     return (<>
-    user list
+    {data.length}
     </>)
 }
 

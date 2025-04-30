3f1138f0c2017417b032d5e916cbfdf04951405d
Sharath44665 <sharathwelcomesyouin@gmail.com>
2025-04-30 21:12:33 +0530

    adding more on loader

--- registry/src/App.tsx
+++ registry/src/App.tsx
@@ -10,18 +10,31 @@ import DetailsPage from './pages/DetailsPage'
 
 const router = createBrowserRouter([
   {
-    path:'/',
+    path: '/',
     element: <Root />,
-    children:[
+    children: [
       {
-        index:true,
+        index: true,
         element: <HomePage />
       },
       {
         path: '/search',
         element: <SearchPage />,
-        loader: () => {
-          return ['react', 'react-redux', 'redux']
+        loader: async ({ request }) => {
+          // console.log(request)
+          const { searchParams } = new URL(request.url)
+          const term = searchParams.get('term')
+
+          if (!term) {
+            throw new Error('search term must be provided')
+          }
+          // console.log(term)
+          const res = await fetch(
+            `https://registry.npmjs.org/-/v1/search?text=${term}`
+          )
+
+          const data = await res.json()
+          return data.objects
         },
       },
       {
@@ -32,14 +45,14 @@ const router = createBrowserRouter([
 
   }
 ])
-function App() { 
+function App() {
 
   return (
     <>
-    <RouterProvider router={router}>
+      <RouterProvider router={router}>
 
-    </RouterProvider>
-     
+      </RouterProvider>
+
     </>
   )
 }
